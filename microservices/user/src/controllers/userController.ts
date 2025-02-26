a2d465d7526a23d0be43e2c148f76825ab4cb043
Giannis <g.karampas91@gmail.com>
2025-02-26 22:39:07 +0200

    Update the routes endpoints for auth,business, review and user

--- microservices/user/src/controllers/userController.ts
+++ microservices/user/src/controllers/userController.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
 import { validationResult } from "express-validator";
 import User from "../models/User.model";
 
